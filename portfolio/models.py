9fc8f69134ffc5054c834588ad99743f2f20ec8e
PraseedaSasankaPisipati <spisipati@unomaha.edu>
2017-07-25 21:14:06 -0500

    pushing the 1p3 assignment

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from yahoo_finance import Share
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
@@ -36,6 +37,7 @@ class Investment(models.Model):
     acquired_date = models.DateField(default=timezone.now)
     recent_value = models.DecimalField(max_digits=10, decimal_places=2)
     recent_date = models.DateField(default=timezone.now, blank=True, null=True)
+	
 
     def created(self):
         self.acquired_date = timezone.now()
@@ -49,7 +51,9 @@ class Investment(models.Model):
         return str(self.customer)
 
     def results_by_investment(self):
-        return self.recent_value - self.acquired_value
+        return self.recent_value - self.acquired_value	
+		
+		
 		
 class Stock(models.Model):
     customer = models.ForeignKey(Customer, related_name='stocks')
@@ -68,3 +72,14 @@ class Stock(models.Model):
 
     def initial_stock_value(self):
         return self.shares * self.purchase_price
+    def current_stock_price(self):
+        symbol_f = self.symbol
+        data = Share(symbol_f)
+        share_value = (data.get_open())
+        return share_value
+
+    def current_stock_value(self):
+        symbol_f = self.symbol
+        data = Share(symbol_f)
+        share_value = (data.get_open())
+        return float(share_value) * float(self.shares)
